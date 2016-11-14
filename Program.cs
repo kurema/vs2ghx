9c39f2f2839485ffdde8d8abcd84c1f9c337188d
kurema <kurema_makoto_software@yahoo.co.jp>
2016-11-14 12:12:37 +0900

    Initial

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+namespace vs2ghx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var basicPath = Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), "base.ghx");
+            string ghxBase = "";
+            if (File.Exists(basicPath)) {
+                ghxBase = File.ReadAllText(basicPath);
+            }else
+            {
+                var stream = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream("vs2ghx.base.ghx");
+                var sr = new StreamReader(stream);
+                ghxBase = sr.ReadToEnd();
+            }
+
+            var resultBuilder = new StringBuilder();
+            foreach(var item in args)
+            {
+                if (File.Exists(item))
+                {
+                    var text = System.IO.File.ReadAllText(item,Encoding.UTF8);
+                    resultBuilder.Append("#region " + item + "\r\n");
+                    var regex = new System.Text.RegularExpressions.Regex(@"namespace (.+?)\{(.+)\}", System.Text.RegularExpressions.RegexOptions.Singleline);
+                    var match= regex.Match(text);
+                    if (!match.Success)
+                    {
+                        Console.Error.WriteLine("regex fail.");
+                    }
+                    resultBuilder.Append(match.Groups[2]);
+                    resultBuilder.Append("#endregion\r\n\r\n");
+                }
+            }
+
+            ghxBase = ghxBase.Replace("[--Additional Source--]", System.Security.SecurityElement.Escape(resultBuilder.ToString()));
+            ghxBase = ghxBase.Replace("[--Main Program--]", "");
+
+            Console.Write(ghxBase);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,60 @@
 # vs2ghx
 複数の*.csファイルからGrasshopperファイルを作成する簡単なツールです。
+## 使い方
+```
+vs2ghx a.cs b.cs > out.ghx
+```
+引数にcsファイルをいくつか指定し、適当なファイルにリダイレクトします。
+
+ghxのベースとなるファイルは存在すれば実行ファイルと同じフォルダに或るbase.ghxを、なければ埋め込まれたリソースを利用します。
+そのいずれかのフォルダのうち、``[--Additional Source--]``と書かれた部分を*.csファイルの内容で置き換えます。
+## 利用目的
+GrasshopperのCSコンポーネントには補完機能がついていますが貧弱です。
+VisualStudioの補完機能とエラーチェックを利用してGrasshopper用のライブラリを作成する為に作ったツールです。
+
+Perlで作れば十分だったと思いますが、他人が使う場合はPerlが入っているか怪しいので.netで作りました。
+## 注意
+*.csファイルのうち、外側のnamespaceの内部のみ結合します。
+従って、namespaceを入れ子にしないでください。
+
+正しい例
+```
+using Rhino;
+using Rhino.Geometry;
+using Rhino.DocObjects;
+using Rhino.Collections;
+
+...
+
+namespace kurema.RhinoTools
+{
+  public class LSystem
+  {
+    ...
+  }
+}
+```
+
+上手くいかない例
+```
+using Rhino;
+using Rhino.Geometry;
+using Rhino.DocObjects;
+using Rhino.Collections;
+
+...
+
+namespace kurema
+{
+  namespace RhinoTools{
+    public class LSystem
+    {
+      ...
+    }
+  }
+}
+```
+## アドバイス
+VisualStudioのプロジェクトのプロパティを開き、ビルドイベントにてビルド後に毎回実行されるようにすれば自動で同期されて便利です。
+なお、この際のカレントディレクトリは実行ファイルのある場所です。(bin/debug等)
+ただし、作成したghxファイルを編集後は別のファイル名で保存するようにしましょう。
